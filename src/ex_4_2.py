191c5feb47582007fa5e3c0aa7cec3fe3dd1a267
Sai Nikhil Krosuri <110455764+sainikhilkrosuri@users.noreply.github.com>
2023-12-08 23:57:33 -0600

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 
 def logstamp_to_datetime(datestr):
-    datestr = '2023-12-08T10:30:00'
     formated_str = '%Y-%m-%dT%H:%M:%S'
     final_dt = datetime.strptime(datestr, formated_str)
     return final_dt
